f549ed865e15048d965285c63dec1fa11b7e2252
Gustavo Leme <gleme@nevada.unr.edu>
2020-06-20 15:16:41 -0300

    adds services exports

---
+++ src/services/index.js
@@ -0,0 +1,11 @@
+const auth = require('./auth');
+const database = require('./database');
+const drone = require('./drone');
+const users = require('./users');
+
+module.exports = {
+    auth,
+    database,
+    drone,
+    users,
+};
