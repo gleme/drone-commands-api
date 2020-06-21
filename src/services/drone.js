3dce1034f1eb55c288ab6fc4b6420d78d5035d1a
Gustavo Leme <gleme@nevada.unr.edu>
2020-06-21 16:40:34 -0300

    fixes status update to atomic operations on mongodb

--- src/services/drone.js
+++ src/services/drone.js
@@ -1,24 +1,44 @@
 const { spawn } = require('child_process');
-const { Inspection, InspectionStatus } = require('../domain/models');
+const _ = require('lodash');
+const { Invocation, InvocationStatus } = require('../domain/models');
 
-async function invokeCommand(command, inspection) { 
+async function invokeCommand(command, invocation) { 
 
     const cmdProcess = spawn(command.executablePath, command.arguments);
     cmdProcess.on('error', async error => {
-        // TODO: update command invocation status and notify user
-        await Inspection.updateOne({ _id: inspection._id }, { $set: { status: InspectionStatus.ERROR, result: error } });
-    });
-    cmdProcess.on('close', async code => {
-        console.log('close');
-        await Inspection.updateOne({ _id: inspection._id }, { $set: { status: InspectionStatus.FINISHED_EXECUTION, result: String(code) } });
-    });
-    cmdProcess.on('exit', code => {
-        console.log('exit', code);
-        // TODO: update command invocation status and notify user
+        // TODO: notify user
+        await Invocation.update({ _id: invocation._id }, [{
+            $set: {
+                result: { $concat: ['$result', `\nerror: ${error}`] },
+                status: InvocationStatus.ERROR,
+            }
+        }]);
     });
 
-    // TODO: update command invocation status
-    await Inspection.updateOne({ _id: inspection._id }, { $set: { status: InspectionStatus.EXECUTING } });
+    cmdProcess.on('close', async code => {
+        console.log('close:', code);
+        await Invocation.update({ _id: invocation._id }, [{
+            $set: {
+                result: { $concat: ['$result', `\ncode: ${code}`] },
+                status: InvocationStatus.FINISHED_EXECUTION,
+            }
+        }]);
+    });
+
+    cmdProcess.on('exit', code => {
+        // TODO: update command invocation status and notify user
+        console.log('exit:', code);
+    });
+    
+    cmdProcess.stdout.on('data', async data => {
+        await Invocation.update({ _id: invocation._id }, [{
+            $set: {
+                result: { $concat: ['$result', `\n${data.toString()}`] },
+            },
+        }]);
+    });
+
+    await Invocation.updateOne({ _id: invocation._id }, { $set: { status: InvocationStatus.EXECUTING } });
 }
 
 module.exports = {
